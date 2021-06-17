fd05188548bf56ac42d72bedb661a45d23d1bac9
hakase-monoshiri <ebenaeleazer@outlook.com>
2021-06-16 21:58:57 -0400

    Done.

--- index.js
+++ index.js
@@ -14,12 +14,19 @@ function handleClick(e) {
 
 function greet (timeString) {
   let time = timeString.split(":");
-  let hour = time[0];
+  const hour = time[0];
   if (hour < 12) {
-   return "Good Morning.";
+   return "Good Morning";
   } else if (time[0] >= 12 && time[0] < 17) {
-    return "Good Afternoon.";
+    return "Good Afternoon";
   }else {
-    return "Good Evening.";}
+    return "Good Evening";}
+
+}
+
+function displayMessage (string) {
+
+  let greeting = string;
+  document.getElementById("greeting").innerText = greeting;
 
 }
